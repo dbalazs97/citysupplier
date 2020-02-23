396eefe3581dde79ffbce6cb582c7dac94c1739b
Dóka Balázs <dbalazs40@gmail.com>
2020-02-23 18:42:17 +0100

    Add test for getAtPosition an setAtPosition neighbours

--- src/model/map/Chunk.test.ts
+++ src/model/map/Chunk.test.ts
@@ -1,5 +1,6 @@
 import { Chunk, CHUNK_SIZE } from './Chunk';
 import { Entity } from '../entity/Entity';
+import { Direction } from '../general/Direction';
 
 let chunk: Chunk;
 
@@ -17,7 +18,7 @@ describe('Generation', () => {
 	});
 });
 
-describe('getAtPosition', () => {
+describe('setAtPosition', () => {
 	it('should should return entity from given position', () => {
 		const entity = new Entity({ x: 0, y: 0 });
 		chunk.setAtPosition(0, 0, entity);
@@ -45,5 +46,53 @@ describe('getAtPosition', () => {
 
 	it('should throw error on big y', () => {
 		expect(() => chunk.setAtPosition(0, CHUNK_SIZE + 1, {} as Entity)).toThrow(RangeError);
+	});
+
+	it('should set neighbours for 1 entity', () => {
+		const entity = new Entity({ x: 0, y: 0 });
+		chunk.setAtPosition(0, 0, entity);
+		expect(entity.neighbours.get(Direction.UP)).toBe(null);
+		expect(entity.neighbours.get(Direction.DOWN)).toBe(null);
+		expect(entity.neighbours.get(Direction.LEFT)).toBe(null);
+		expect(entity.neighbours.get(Direction.RIGHT)).toBe(null);
+	});
+
+	it('should set neighbours for 2 entities next to each other', () => {
+		const entityA = new Entity({ x: 0, y: 0 });
+		const entityB = new Entity({ x: 1, y: 0 });
+		chunk.setAtPosition(0, 0, entityA);
+		chunk.setAtPosition(1, 0, entityB);
+
+		expect(entityA.neighbours.get(Direction.UP)).toBe(null);
+		expect(entityA.neighbours.get(Direction.DOWN)).toBe(null);
+		expect(entityA.neighbours.get(Direction.LEFT)).toBe(null);
+		expect(entityA.neighbours.get(Direction.RIGHT)).toBe(entityB);
+
+		expect(entityB.neighbours.get(Direction.UP)).toBe(null);
+		expect(entityB.neighbours.get(Direction.DOWN)).toBe(null);
+		expect(entityB.neighbours.get(Direction.LEFT)).toBe(entityA);
+		expect(entityB.neighbours.get(Direction.RIGHT)).toBe(null);
+	});
+});
+
+describe('getAtPosition', () => {
+	it('should return at valid position', () => {
+		expect(chunk.getAtPosition(0, 0)).toBe(null);
+	});
+
+	it('should throw error on negative x', () => {
+		expect(() => chunk.getAtPosition(-1, 0)).toThrow(RangeError);
+	});
+
+	it('should throw error on negative y', () => {
+		expect(() => chunk.getAtPosition(0, -1)).toThrow(RangeError);
+	});
+
+	it('should throw error on big x', () => {
+		expect(() => chunk.getAtPosition(CHUNK_SIZE + 1, 0)).toThrow(RangeError);
+	});
+
+	it('should throw error on big y', () => {
+		expect(() => chunk.getAtPosition(0, CHUNK_SIZE + 1)).toThrow(RangeError);
 	});
 });
