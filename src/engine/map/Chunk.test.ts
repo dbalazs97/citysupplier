b4e1bdd291691c2aea3b42c758cfc719c63ddedd
balazs <dbalazs40@gmail.com>
2020-02-28 11:09:59 +0100

    Add chunk tests for building

--- src/engine/map/Chunk.test.ts
+++ src/engine/map/Chunk.test.ts
@@ -1,11 +1,25 @@
 import { Chunk, CHUNK_SIZE } from './Chunk';
-import { Entity } from '../entity/Entity';
-import { Direction } from '../general/Direction';
+import { Entity } from '../../model/entity/Entity';
+import { Direction } from '../../model/general/Direction';
+import { Building } from '../../model/entity/Building';
+import { Recipe } from '../../model/resource/Recipe';
+import { ResourceType } from '../../model/resource/Resource';
 
 let chunk: Chunk;
+let building: Building;
 
 beforeEach(() => {
 	chunk = new Chunk();
+	building = new class extends Building {
+		public recipe: Recipe = {
+			input: [{ type: ResourceType.Wood, amount: 1 }],
+			output: [{ type: ResourceType.Planks, amount: 1 }],
+		};
+
+		constructor() {
+			super({ x: 0, y: 0 });
+		}
+	};
 });
 
 describe('Generation', () => {
@@ -28,8 +42,8 @@ describe('setAtPosition', () => {
 	it('should change entity position at placing', () => {
 		const entity = new Entity({ x: 1, y: 1 });
 		chunk.setAtPosition(0, 0, entity);
-		expect(chunk.getAtPosition(0, 0).position.x).toBe(0);
-		expect(chunk.getAtPosition(0, 0).position.y).toBe(0);
+		expect(chunk.getAtPosition(0, 0)?.position.x).toBe(0);
+		expect(chunk.getAtPosition(0, 0)?.position.y).toBe(0);
 	});
 
 	it('should throw error on negative x', () => {
@@ -73,6 +87,37 @@ describe('setAtPosition', () => {
 		expect(entityB.neighbours.get(Direction.LEFT)).toBe(entityA);
 		expect(entityB.neighbours.get(Direction.RIGHT)).toBe(null);
 	});
+
+	it('should set neighbours for 4 entities next to each other', () => {
+		const entityA = new Entity({ x: 0, y: 0 });
+		const entityB = new Entity({ x: 1, y: 0 });
+		const entityC = new Entity({ x: 0, y: 1 });
+		const entityD = new Entity({ x: 1, y: 1 });
+		chunk.setAtPosition(0, 0, entityA);
+		chunk.setAtPosition(1, 0, entityB);
+		chunk.setAtPosition(0, 1, entityC);
+		chunk.setAtPosition(1, 1, entityD);
+
+		expect(entityA.neighbours.get(Direction.UP)).toBe(null);
+		expect(entityA.neighbours.get(Direction.DOWN)).toBe(entityC);
+		expect(entityA.neighbours.get(Direction.LEFT)).toBe(null);
+		expect(entityA.neighbours.get(Direction.RIGHT)).toBe(entityB);
+
+		expect(entityB.neighbours.get(Direction.UP)).toBe(null);
+		expect(entityB.neighbours.get(Direction.DOWN)).toBe(entityD);
+		expect(entityB.neighbours.get(Direction.LEFT)).toBe(entityA);
+		expect(entityB.neighbours.get(Direction.RIGHT)).toBe(null);
+
+		expect(entityC.neighbours.get(Direction.UP)).toBe(entityA);
+		expect(entityC.neighbours.get(Direction.DOWN)).toBe(null);
+		expect(entityC.neighbours.get(Direction.LEFT)).toBe(null);
+		expect(entityC.neighbours.get(Direction.RIGHT)).toBe(entityD);
+
+		expect(entityD.neighbours.get(Direction.UP)).toBe(entityB);
+		expect(entityD.neighbours.get(Direction.DOWN)).toBe(null);
+		expect(entityD.neighbours.get(Direction.LEFT)).toBe(entityC);
+		expect(entityD.neighbours.get(Direction.RIGHT)).toBe(null);
+	});
 });
 
 describe('getAtPosition', () => {
@@ -94,5 +139,22 @@ describe('getAtPosition', () => {
 
 	it('should throw error on big y', () => {
 		expect(() => chunk.getAtPosition(0, CHUNK_SIZE + 1)).toThrow(RangeError);
+	});
+});
+
+describe('placeBuilding', () => {
+	it('should place the building to the right place', () => {
+		chunk.placeBuilding(1, 1, building);
+
+		expect(building.position.x).toBe(1);
+		expect(building.position.y).toBe(1);
+		expect(chunk.getAtPosition(1, 1)).toBe(building);
+	});
+
+	it('should properly set chunk balance', () => {
+		chunk.placeBuilding(1, 1, building);
+
+		expect(chunk.getBalance(ResourceType.Wood)).toBe(-1);
+		expect(chunk.getBalance(ResourceType.Planks)).toBe(1);
 	});
 });
