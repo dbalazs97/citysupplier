094205d110d80d6c9d58ae12831385a23504af27
dbalazs97 <dbalazs40@gmail.com>
2020-03-31 23:07:42 +0200

    Fix lint errors

--- common/model/entity/building/Building.ts
+++ common/model/entity/building/Building.ts
@@ -2,8 +2,8 @@ import { Entity } from "../Entity";
 import { ResourceType } from "../../global";
 
 export interface Building extends Entity {
-	inputs: Array<{ type: ResourceType, baseConsumption: number }>;
-	outputs: Array<{ type: ResourceType, baseProduction: number }>;
+	inputs: Array<{ type: ResourceType; baseConsumption: number }>;
+	outputs: Array<{ type: ResourceType; baseProduction: number }>;
 
 	level: number;
 	levelModifier: number;
